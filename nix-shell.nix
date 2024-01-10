ef6664d190b07bf5f1f9b46d5d7cd8f10206ffa9
mytkom <marek.mytkowski.mm@gmail.com>
2024-01-10 12:10:16 +0100

    first cpu levenstein distance implementation

---
+++ nix-shell.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+   name = "cuda-env-shell";
+   buildInputs = with pkgs; [
+     git gitRepo gnupg autoconf curl
+     procps gnumake util-linux m4 gperf unzip
+     cudatoolkit linuxPackages.nvidia_x11
+     xorg.libXi xorg.libXmu
+     xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
+     ncurses5
+   ];
+   nativeBuildInputs = with pkgs; [
+     clang-tools
+     bear
+     pkg-config
+   ];
+   shellHook = ''
+      export CUDA_PATH=${pkgs.cudatoolkit}
+      # export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib
+      export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+      export EXTRA_CCFLAGS="-I/usr/include"
+   '';          
+}
